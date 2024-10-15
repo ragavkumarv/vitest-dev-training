3f402cc2040c4bd5e057d9234d0bfa65d4ac3230
ragavkumarv <ragavkumarv@gmail.com>
2024-10-15 11:51:48 +0530

    Added error check

--- src/deep-merge.ts
+++ src/deep-merge.ts
@@ -1,12 +1,16 @@
 export function deepMerge(a, b) {
-  if (Array.isArray(a)) {
-    return [...a, ...b];
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return [...a, ...b]
   }
-  const merged = { ...a };
+  if (Array.isArray(a) || Array.isArray(b) || typeof a !== typeof b) {
+    throw new Error('Can not merge two differnet types')
+  }
+  const merged = { ...a }
   for (const key of Object.keys(b)) {
     if (typeof a[key] === 'object' || Array.isArray(a[key]))
-      merged[key] = deepMerge(a[key], b[key]);
-    else merged[key] = b[key];
+      merged[key] = deepMerge(a[key], b[key])
+    else
+      merged[key] = b[key]
   }
-  return merged;
+  return merged
 }
